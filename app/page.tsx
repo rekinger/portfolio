255925bddd7c79ac60ad412e5bf09c8479381461
rekinger <rpkingcsc@gmail.com>
2024-06-23 20:39:29 -0600

    please

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <Tabs classNames={{
         tabList: isMobile ? "gap-10 w-screen": "gap-10 w-full",
       }} 
-      key={"nav tabs"} variant={"underlined"} isVertical={window.innerWidth > 600} size={"lg"} aria-label="Tabs variants">
+      key={"nav tabs"} variant={"underlined"} isVertical={global?.window ? window.innerWidth > 600: false} size={"lg"} aria-label="Tabs variants">
         <Tab key="Me" title="Me">
           <Card className="max-w-[400px]">
             <CardHeader className="flex gap-3">
